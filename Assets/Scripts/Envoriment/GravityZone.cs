eb3f47431defa1c3f11f856fe362b26ad69b0f3e
Tenseven-107 <nadrichem@tanous.nl>
2025-02-04 23:44:52 +0100

    Finished level 1 its design

--- Assets/Scripts/Envoriment/GravityZone.cs
+++ Assets/Scripts/Envoriment/GravityZone.cs
@@ -1,11 +1,22 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
 
 public class GravityZone : MonoBehaviour
 {
     private readonly List<Rigidbody2D> bodies = new();
+    
+    [SerializeField] private Vector2 size = Vector2.one;
+    public Vector2 Size { get => size; set { size = value; ScaleRect(); } }
 
+    [SerializeField] private BoxCollider2D trigger;
+    [SerializeField] private SpriteRenderer sprite;
+    [SerializeField] private Light2D lightBox;
+    
     private void FixedUpdate()
     {
         ApplyGravity();
@@ -35,5 +46,22 @@ public class GravityZone : MonoBehaviour
         {
             body.AddForce(transform.up * -Physics2D.gravity, ForceMode2D.Force);
         }
+    }
+
+
+    private void OnValidate() { ScaleRect(); }
+    private void ScaleRect()
+    {
+        trigger.size = size;
+        sprite.size = size;
+
+        lightBox.lightType = Light2D.LightType.Freeform;
+        var lightShape = new Vector3[4];
+        lightShape[0] = (new Vector3(size.x / 2, size.y / 2));
+        lightShape[1] = new Vector3(size.x / 2, -(size.y / 2));
+        lightShape[2] = new Vector3(-(size.x / 2), -(size.y / 2));
+        lightShape[3] = new Vector3(-(size.x / 2), size.y / 2);
+
+        lightBox.SetShapePath(lightShape);
     }
 }
